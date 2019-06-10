a097311b4b6fc68ff3bd10e307cdf8eced066174
Medi Assumani <mediassumani49@gmail.com>
2019-06-10 15:27:21 -0700

    [Update] html files

--- pizzashop/pizza/models.py
+++ pizzashop/pizza/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Topping(models.Model):
+    pass
+
+class PizzaTopping(models.Model):
+    topping = models.ForeignKey('Topping', on_delete=models.PROTECT)
+    pizza = models.ForeignKey('Pizza', on_delete=models.PROTECT)
+
+class Pizza(models.Model):
+    
+    name = models.CharField(max_length=60)
+    toppings = models.ForeignKey('Topping', through=PizzaTopping)
+
+    def __str__(self):
+        return self.name
