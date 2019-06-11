17700ea25f5f250a00fe0912a1460aea35c019ed
Medi Assumani <mediassumani49@gmail.com>
2019-06-11 09:49:24 -0700

    [Update] registered pizza models for admin portal

--- pizzashop/pizza/admin.py
+++ pizzashop/pizza/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pizza.models import Pizza, PizzaOrder, Topping
+
 
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(PizzaOrder)
+admin.site.register(Topping)
